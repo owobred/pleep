03913f5ea8f59afb587061d309965f42a9b3e409
owobred <29340449+owobred@users.noreply.github.com>
2024-06-19 22:15:08 +0100

    add some audio loading and resampling

---
+++ pleep-audio/src/lib.rs
@@ -0,0 +1,134 @@
+use rubato::Resampler;
+use symphonia::core::{
+    audio::AudioBuffer,
+    codecs::DecoderOptions,
+    conv::FromSample,
+    formats::FormatOptions,
+    io::MediaSourceStream,
+    meta::MetadataOptions,
+    probe::Hint,
+    sample::Sample,
+};
+use thiserror::Error;
+use tracing::instrument;
+
+pub trait AnySample:
+    Sample
+    + FromSample<u8>
+    + FromSample<u16>
+    + FromSample<symphonia::core::sample::u24>
+    + FromSample<u32>
+    + FromSample<i8>
+    + FromSample<i16>
+    + FromSample<symphonia::core::sample::i24>
+    + FromSample<i32>
+    + FromSample<f32>
+    + FromSample<f64>
+{
+}
+
+impl<
+        T: Sample
+            + FromSample<u8>
+            + FromSample<u16>
+            + FromSample<symphonia::core::sample::u24>
+            + FromSample<u32>
+            + FromSample<i8>
+            + FromSample<i16>
+            + FromSample<symphonia::core::sample::i24>
+            + FromSample<i32>
+            + FromSample<f32>
+            + FromSample<f64>,
+    > AnySample for T
+{
+}
+
+pub trait ExtendedAnySample: AnySample + rubato::Sample {}
+impl<T: AnySample + rubato::Sample> ExtendedAnySample for T {}
+
+#[instrument(skip(media_source), err(level = "debug"), level = "trace")]
+pub fn load_audio<T: ExtendedAnySample>(
+    media_source: MediaSourceStream,
+) -> Result<Audio<T>, Error> {
+    let registry = symphonia::default::get_codecs();
+    let probe = symphonia::default::get_probe();
+    let mut format = probe.format(
+        &Hint::new(),
+        media_source,
+        &FormatOptions::default(),
+        &MetadataOptions::default(),
+    )?;
+
+    let default_track = format.format.default_track().expect("no default track");
+    let default_track_id = default_track.id;
+    let default_track_params = default_track.codec_params.to_owned();
+
+    let mut decoder = registry.make(&default_track_params, &DecoderOptions::default())?;
+
+    let mut samples = Vec::new();
+
+    while let Ok(packet) = format.format.next_packet() {
+        if packet.track_id() != default_track_id {
+            continue;
+        }
+
+        let audio_buffer = decoder.decode(&packet)?;
+        let mut float_converted: AudioBuffer<T> =
+            AudioBuffer::new(audio_buffer.frames() as u64, audio_buffer.spec().to_owned());
+        audio_buffer.convert(&mut float_converted);
+        drop(audio_buffer);
+        let planes = float_converted.planes();
+        let planes_slice = planes.planes();
+        let main_channel = planes_slice[0];
+
+        samples.extend(main_channel);
+    }
+
+    Ok(Audio {
+        sample_rate: default_track_params.sample_rate.unwrap() as usize,
+        samples,
+    })
+}
+
+#[derive(Debug)]
+pub struct Audio<T: AnySample> {
+    pub samples: Vec<T>,
+    pub sample_rate: usize,
+}
+
+pub fn resample_audio<T: ExtendedAnySample>(
+    audio: Audio<T>,
+    settings: &ResampleSettings,
+) -> Result<Audio<T>, Error> {
+    let mut resampler = rubato::FftFixedIn::new(
+        audio.sample_rate,
+        settings.target_sample_rate,
+        audio.samples.len(),
+        settings.sub_chunks,
+        1,
+    )?;
+    let resampled = resampler.process(&[&audio.samples], None)?;
+
+    Ok(Audio {
+        samples: resampled.into_iter().next().unwrap(),
+        sample_rate: settings.target_sample_rate,
+    })
+}
+
+#[derive(Debug)]
+pub struct ResampleSettings {
+    pub target_sample_rate: usize,
+    pub sub_chunks: usize,
+}
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("error passed from symphonia: {0:?}")]
+    Symphonia(#[from] symphonia::core::errors::Error),
+    #[error("audio did not have a default track")]
+    NoDefaultTrack,
+    #[error("error constructing resampler: {0:?}")]
+    ResamplerConstruction(#[from] rubato::ResamplerConstructionError),
+    #[error("error resampling: {0:?}")]
+    Resampler(#[from] rubato::ResampleError),
+}
--- pleep/src/spectrogram.rs
+++ pleep/src/spectrogram.rs
@@ -1,7 +1,11 @@
-use std::sync::{Arc, Mutex};
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex, RwLock},
+};
 
 use num_complex::Complex;
 use rustfft::{Fft, FftPlanner};
+use tracing::instrument;
 
 pub trait Float: rustfft::FftNum + num_traits::Float {}
 impl Float for f64 {}
@@ -10,12 +14,14 @@ impl Float for f32 {}
 #[derive(Clone)]
 pub struct Generator<T: Float> {
     fft_planner: Arc<Mutex<FftPlanner<T>>>,
+    hanns: Arc<RwLock<HashMap<usize, Arc<Vec<T>>>>>,
 }
 
 impl<T: Float> Generator<T> {
     pub fn new() -> Self {
         Self {
             fft_planner: Arc::new(Mutex::new(rustfft::FftPlanner::new())),
+            hanns: Default::default(),
         }
     }
 
@@ -23,6 +29,7 @@ impl<T: Float> Generator<T> {
         let fft = self.get_forward_fft(settings.fft_len);
         let scratch_size = fft.get_outofplace_scratch_len();
         let mut scratch = vec![Complex::new(T::zero(), T::zero()); scratch_size];
+        let hann = self.get_hann(settings.fft_len);
 
         let spectrogram = samples
             .into_iter()
@@ -32,6 +39,10 @@ impl<T: Float> Generator<T> {
             .step_by(settings.fft_len - settings.fft_overlap)
             .map(|sample_group| {
                 let mut group = sample_group.to_vec();
+                group
+                    .iter_mut()
+                    .zip(hann.iter())
+                    .for_each(|(sample, &frac)| sample.re = sample.re * frac);
                 fft.process_with_scratch(&mut group, &mut scratch);
                 group
             })
@@ -52,6 +63,40 @@ impl<T: Float> Generator<T> {
 
         planner.plan_fft_forward(len)
     }
+
+    fn get_hann(&self, size: usize) -> Arc<Vec<T>> {
+        let read = self.hanns.read().unwrap();
+
+        match read.contains_key(&size) {
+            true => read.get(&size).unwrap().to_owned(),
+            false => {
+                drop(read);
+                self.generate_hann(size)
+            }
+        }
+    }
+
+    #[instrument(skip(self), level = "trace")]
+    fn generate_hann(&self, size: usize) -> Arc<Vec<T>> {
+        let hann = generate_hanning_window(size);
+        let hann = Arc::new(hann);
+        let mut write = self.hanns.write().unwrap();
+        write.insert(size, hann.clone());
+        hann
+    }
+}
+
+fn generate_hanning_window<T: Float>(size: usize) -> Vec<T> {
+    let half = T::from(0.5).unwrap();
+    let tau = T::from(std::f64::consts::TAU).unwrap();
+
+    let mut out = vec![T::zero(); size];
+
+    for i in 0..size {
+        out[i] = half * (T::one() - (tau * (T::from(i).unwrap() / T::from(size).unwrap())).cos());
+    }
+
+    out
 }
 
 #[derive(Debug, Clone)]
