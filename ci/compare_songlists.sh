1ccd626224007344ccac48f47a41f21ffe61d579
owobred <29340449+owobred@users.noreply.github.com>
2025-08-07 11:11:18 +0100

    use the right output in the action

--- ci/compare_songlists.sh
+++ ci/compare_songlists.sh
@@ -4,7 +4,7 @@ exit_code=$?
 if [ $exit_code -eq 0 ]
 then
     echo "songlist did not change"
-    echo "songlist_changed=0" >> "$GITHUB_OUTPUT"
+    echo "songlist_updated=0" >> "$GITHUB_OUTPUT"
 else
     echo "songlist changed"
     mv new_songlist.csv songlist/songlist.csv
@@ -16,5 +16,5 @@ else
     git add songlist.csv
     git commit -m "update songlist"
     git push origin songlist-history
-    echo "songlist_changed=1" >> "$GITHUB_OUTPUT"
+    echo "songlist_updated=1" >> "$GITHUB_OUTPUT"
 fi
